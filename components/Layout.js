9a7460ca385accf68d3c7dac53f09620d658cdf9
Yakushin103 <vyakushin103@yandex.ru>
2021-12-15 16:08:07 +0300

    Added switch component for choose theme

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import Head from 'next/head'
 
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import AppBar from '@mui/material/AppBar'
@@ -14,11 +16,39 @@ import MenuItem from '@mui/material/MenuItem'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
+import UseSwitchesCustom from './UseSwitchesCustom'
 
 import { pages } from '../utils/constants'
 
 export default function Layout({ title, description, children }) {
-  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElNav, setAnchorElNav] = useState(null)
+  const [darkMode, setDarkMode] = useState(false)
+
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0'
+      },
+      h2: {
+        fontSize: '4rem',
+        margin: '1rem 0'
+      },
+      body1: {
+        fontWeight: 'normal',
+      }
+    },
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+      primary: {
+        main: darkMode ? '#fff' : '#3a3a3a'
+      },
+      secondary: {
+        main: '#ccc'
+      }
+    }
+  })
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget)
@@ -26,6 +56,10 @@ export default function Layout({ title, description, children }) {
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null)
+  }
+
+  const darkHandler = () => {
+    setDarkMode(!darkMode)
   }
 
   return (
@@ -39,160 +73,170 @@ export default function Layout({ title, description, children }) {
           <meta name="description" content={description}></meta>
         }
       </Head>
-
-      <AppBar position="static">
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-
-            <Grid
-              direction="column"
-              container
-            >
-              <Grid sx={{ maxHeight: '35px' }} item>
-                <Grid
-                  sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center' }}
-                  container
-                >
-                  <Grid item xs={4}></Grid>
-
-                  <Grid
-                    item
-                    style={{ textAlign: 'center' }}
-                    xs={4}
-                  >
-                    <Typography
-                      variant="h6"
-                      noWrap
-                      component="h6"
-                    >
-                      LOGO
-                    </Typography>
-                  </Grid>
-
-                  <Grid
-                    sx={{ textAlign: 'end' }}
-                    item
-                    xs={4}
-                  >
-                    <Button>
-                      <FavoriteBorderIcon sx={{ color: 'white' }} />
-                    </Button>
-
-                    <Button>
-                      <AccountCircleIcon sx={{ color: 'white' }} />
-                    </Button>
-
-                    <Button>
-                      <ShoppingCartIcon sx={{ color: 'white' }} />
-                    </Button>
-                  </Grid>
-                </Grid>
-
-                <Grid
-                  sx={{ display: { xs: 'flex', md: 'none' }, alignItems: 'center' }}
-                  container
-                >
-                  <Grid
-                    sx={{ textAlign: 'start' }}
-                    xs={4}
-                  >
-                    <IconButton
-                      size="large"
-                      aria-label="account of current user"
-                      aria-controls="menu-appbar"
-                      aria-haspopup="true"
-                      onClick={handleOpenNavMenu}
-                      color="inherit"
-                    >
-                      <MenuIcon />
-                    </IconButton>
-                    <Menu
-                      id="menu-appbar"
-                      anchorEl={anchorElNav}
-                      anchorOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'left',
-                      }}
-                      keepMounted
-                      transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'left',
-                      }}
-                      open={Boolean(anchorElNav)}
-                      onClose={handleCloseNavMenu}
-                      sx={{
-                        display: { xs: 'block', md: 'none' },
-                      }}
-                    >
-                      {pages.map((page) => (
-                        <MenuItem key={page.name} onClick={handleCloseNavMenu}>
-                          <Typography textAlign="center">{page.value}</Typography>
-                        </MenuItem>
-                      ))}
-                    </Menu>
-
-                    <Button>
-                      <FavoriteBorderIcon sx={{ color: 'white' }} />
-                    </Button>
-                  </Grid>
-
-                  <Grid
-                    sx={{ textAlign: 'center' }}
-                    xs={4}
-                  >
-                    <Typography
-                      variant="h6"
-                      noWrap
-                      component="h6"
-                    >
-                      LOGO
-                    </Typography>
-                  </Grid>
-
-                  <Grid
-                    sx={{ textAlign: 'end' }}
-                    xs={4}
-                  >
-                    <Button>
-                      <AccountCircleIcon sx={{ color: 'white' }} />
-                    </Button>
-
-                    <Button>
-                      <ShoppingCartIcon sx={{ color: 'white' }} />
-                    </Button>
-                  </Grid>
-                </Grid>
-              </Grid>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar sx={{ backgroundColor: darkMode ? 'black' : 'white' }} position="static">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
 
               <Grid
-                sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'space-between' }}
-                item
+                direction="column"
+                container
               >
-                {pages.map((page) => (
-                  <Button
-                    key={page.name}
-                    onClick={handleCloseNavMenu}
-                    sx={{ my: 2, color: 'white', display: 'block' }}
+                <Grid sx={{ maxHeight: '35px' }} item>
+                  <Grid
+                    sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center' }}
+                    container
                   >
-                    {page.value}
-                  </Button>
-                ))}
+                    <Grid item xs={4}>
+                      <UseSwitchesCustom
+                        checked={darkMode}
+                        darkHandler={darkHandler}
+                      />
+
+                    </Grid>
+
+                    <Grid
+                      item
+                      style={{ textAlign: 'center' }}
+                      xs={4}
+                    >
+                      <Typography
+                        variant="h6"
+                        noWrap
+                        component="h6"
+                        color="primary"
+                      >
+                        LOGO
+                      </Typography>
+                    </Grid>
+
+                    <Grid
+                      sx={{ textAlign: 'end' }}
+                      item
+                      xs={4}
+                    >
+                      <Button>
+                        <FavoriteBorderIcon color="primary" />
+                      </Button>
+
+                      <Button>
+                        <AccountCircleIcon color="primary" />
+                      </Button>
+
+                      <Button>
+                        <ShoppingCartIcon color="primary" />
+                      </Button>
+                    </Grid>
+                  </Grid>
+
+                  <Grid
+                    sx={{ display: { xs: 'flex', md: 'none' }, alignItems: 'center' }}
+                    container
+                  >
+                    <Grid
+                      sx={{ textAlign: 'start' }}
+                      xs={4}
+                    >
+                      <IconButton
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={handleOpenNavMenu}
+                        color="inherit"
+                      >
+                        <MenuIcon />
+                      </IconButton>
+                      <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorElNav}
+                        anchorOrigin={{
+                          vertical: 'bottom',
+                          horizontal: 'left',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                          vertical: 'top',
+                          horizontal: 'left',
+                        }}
+                        open={Boolean(anchorElNav)}
+                        onClose={handleCloseNavMenu}
+                        sx={{
+                          display: { xs: 'block', md: 'none' },
+                        }}
+                      >
+                        {pages.map((page) => (
+                          <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                            <Typography textAlign="center">{page.value}</Typography>
+                          </MenuItem>
+                        ))}
+                      </Menu>
+
+                      <Button>
+                        <FavoriteBorderIcon color="primary" />
+                      </Button>
+                    </Grid>
+
+                    <Grid
+                      sx={{ textAlign: 'center' }}
+                      xs={4}
+                    >
+                      <Typography
+                        variant="h6"
+                        noWrap
+                        component="h6"
+                        color="primary"
+                      >
+                        LOGO
+                      </Typography>
+                    </Grid>
+
+                    <Grid
+                      sx={{ textAlign: 'end' }}
+                      xs={4}
+                    >
+                      <Button>
+                        <AccountCircleIcon color="primary" />
+                      </Button>
+
+                      <Button>
+                        <ShoppingCartIcon color="primary" />
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Grid>
+
+                <Grid
+                  sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'space-between' }}
+                  item
+                >
+                  {pages.map((page) => (
+                    <Button
+                      key={page.name}
+                      onClick={handleCloseNavMenu}
+                      color="primary"
+                      sx={{ my: 2, display: 'block' }}
+                    >
+                      {page.value}
+                    </Button>
+                  ))}
+                </Grid>
               </Grid>
-            </Grid>
+            </Toolbar>
+          </Container>
+        </AppBar>
 
-          </Toolbar>
+        <Container>
+          {children}
         </Container>
-      </AppBar>
 
-      <Container>
-        {children}
-      </Container>
-
-      <footer>
-        <Typography>
-          {/* Design by Nastya Kornyushina. Created by Vasso Yakushin */}
-        </Typography>
-      </footer>
+        <footer>
+          <Typography sx={{ textAlign: 'center' }} color="primary">
+            Design by Nastya Kornyushina. Created by Vasso Yakushin
+          </Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
+import NextLink from 'next/link'
 
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Card from '@mui/material/Card'
+import CardMedia from '@mui/material/CardMedia'
+import CardActionArea from '@mui/material/CardActionArea'
+import CardContent from '@mui/material/CardContent'
+import Button from '@mui/material/Button'
 import Layout from '../components/Layout'
 
 export default function Home() {
@@ -7,7 +16,77 @@ export default function Home() {
       title="Главная страница"
       description=""
     >
-      <p> Главная страница </p>
+      <Grid className="main" container>
+        <Grid
+          item
+          sx={{ width: '50%', height: '100%' }}
+        >
+          <Box></Box>
+          <Typography
+            variant="h2"
+            component="h2"
+          >
+            Ваш поиск моды начинается здесь
+          </Typography>
+
+          <Grid container spacing={2}>
+            <Grid item xs={4}>
+              <Card>
+                <NextLink href={`/product/1`} passHref>
+                  <CardActionArea>
+                    <CardMedia
+                      title="Name"
+                      component="img"
+                      image="/images/pexels-dima-valkov-3266700.jpg"
+                    />
+
+                    <CardContent>
+                      <Typography>
+                        название
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </NextLink>
+              </Card>
+            </Grid>
+
+            <Grid item xs={4}>
+              <Card>
+                <NextLink href={`/product/1`} passHref>
+                  <CardActionArea>
+                    <CardMedia
+                      title="Name"
+                      component="img"
+                      image="/images/pexels-leah-kelley-1449657.jpg"
+                    />
+
+                    <CardContent>
+                      <Typography>
+                        название
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </NextLink>
+              </Card>
+            </Grid>
+
+            <Grid item xs={4}>
+              <Button color="primary" variant="outlined">
+                <Typography >
+                  посмотреть коллекцию
+                </Typography>
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid
+          item
+          sx={{ width: '50%', height: '100%' }}
+        >
+
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
